14b38dd9b71f09912184997c2e4db441b7fc09a5
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-11-12 22:49:21 +0300

    adding permissions

---
+++ watchlist_app/api/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework import permissions
+
+
+
+class IsAdminOrReadOnly(permissions.IsAdminUser):
+    
+    def has_permission(self, request, view):
+        
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            bool(request.user and request.user.is_staff)
+            
+            
+class ReviewUserOrReadOnly(permissions.BasePermission):
+    
+    def has_object_permission(self, request, view, obj):
+        
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else: 
+            return obj.review_user == request.user
