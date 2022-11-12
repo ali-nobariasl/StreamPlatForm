eae16f1abcd2deec770dec695cc0b06f67fd8810
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-11-12 22:15:50 +0300

    serialzers are fixed

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,23 +1,22 @@
 from rest_framework import serializers
 from watchlist_app.models import StreamPlatform ,WatchList , Review 
 
-  
-
-class WatchListSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = WatchList
-        fields = "__all__"
-         
          
 class ReviewSerializer(serializers.ModelSerializer):
-    
+    review_user = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Review
         fields = "__all__"
 
+ 
+class WatchListSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+    class Meta:
+        model = WatchList
+        fields = "__all__"
+        
 class StreamPlatformSerializer(serializers.ModelSerializer):
-    
+    watchlist = WatchListSerializer(many=True, read_only=True)
     class Meta:
         model = StreamPlatform
         fields = "__all__"
