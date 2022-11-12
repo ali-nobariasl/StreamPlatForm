f8b71c7fda0fefb6c39d463153ed60705bdad5c3
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-11-12 20:09:45 +0300

    a

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path , include
 
 from watchlist_app.api.views import (movie_list , movie_detail, platform_list , platform_Detail ,
-                                     Movie_detailAv , Movie_listAv , platform_listAv
+                                     Movie_detailAv , Movie_listAv , platform_listAv, platform_detailAv
                                        )
 
 
@@ -14,6 +14,6 @@ urlpatterns = [
     path('list/<int:pk>/', Movie_detailAv.as_view(), name = 'movie-detail'),
     
     path('platlist/', platform_listAv.as_view(), name = 'platform-list'),
-    path('platlist/<int:pk>/', platform_Detail, name = 'platform-Detail'),
+    path('platlist/<int:pk>/', platform_detailAv.as_view(), name = 'platform-Detail'),
     
 ]
