441302122f031e922b08f1e81f7ef2c814b55d03
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-11-15 12:56:00 +0300

    a

--- pract/urls.py
+++ pract/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path , include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('watch/', include('watchlist_app.api.urls')),
-    #path('account/',include('user_app.api.urls')),
-    path('api-auth/', include('rest_framework.urls')),
+    path('account/', include('user_app.api.urls')),
+    #path('api-auth/', include('rest_framework.urls')),
 ]
---
+++ user_app/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from user_app.api.views import registerstion_view , logout_view
+
+
+urlpatterns = [
+    path('login/', obtain_auth_token, name ='login'),
+]
