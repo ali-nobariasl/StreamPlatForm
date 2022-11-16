e2aef5a4226b1d929d4b61acd716abc2f089925d
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-11-16 16:56:01 +0300

    log out function

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from user_app.api.views import registerstion_view 
+from user_app.api.views import registerstion_view ,logout_view
 
 
 urlpatterns = [
     path('login/', obtain_auth_token, name ='login'),
-    path('register/', registerstion_view, name ='register'),
+    path('register/', logout_view, name ='register'),
+    path('logout/', registerstion_view, name ='logout'),
     
 ]
--- user_app/api/views.py
+++ user_app/api/views.py
@@ -7,6 +7,12 @@ from user_app.api.serializers import RegisterationSerializer
 from user_app import models
 
 
+@api_view(['POST',])
+def logout_view(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+
 
 @api_view(['POST',])
 def registerstion_view(request):
