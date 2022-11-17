d10a9ab3b001ffed163462f45b30a9533009885e
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-11-17 16:17:37 +0300

    general throttle class

--- pract/settings.py
+++ pract/settings.py
@@ -131,9 +131,18 @@ REST_FRAMEWORK = {
 #   'DEFAULT_PERMISSION_CLASSES':[
 #       'rest_framework.permissions.IsAuthenticated',
 #    ],
-     'DEFAULT_AUTHENTICATION_CLASSES': [
-#       'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+       'rest_framework.authentication.TokenAuthentication',
+#       'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '2/day',
+        'user': '3/day'
+    }
 }
+
 
--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from user_app.api.views import registerstion_view ,logout_view
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+#from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('login/', obtain_auth_token, name ='login'),
