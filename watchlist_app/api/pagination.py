af6fb42f9fdd74adcfd98df1cfdba82d41ed98ef
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-11-18 12:02:18 +0300

    limit-offset

--- watchlist_app/api/pagination.py
+++ watchlist_app/api/pagination.py
@@ -1,10 +1,16 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination , LimitOffsetPagination
 
 
 
 
 class WatchListPagination(PageNumberPagination):
     page_size = 10
-    page_query_param = 'p'
+    #page_query_param = 'p'
     page_size_query_param = 'size'
     max_page_size = 20
+    #last_page_strings = 'end'
+
+class WatchListLOPagination(LimitOffsetPagination):
+    default_limit = 5
+    max_limit =10
+    
